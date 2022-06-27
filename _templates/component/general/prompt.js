cc36f72ea7bc9aeab59b2ea823878056b7b82b32
신성환 <blue@woowahan.com>
2022-06-27 14:19:51 +0900

    fix: fix CLI comment as proper messgae (snake-case -> kebab-case)

--- _templates/component/general/prompt.js
+++ _templates/component/general/prompt.js
@@ -6,14 +6,14 @@ module.exports = {
       .prompt({
         type: 'input',
         name: 'name',
-        message: '컴포넌트 이름을 snake-case 로 입력하세요.'
+        message: '컴포넌트 이름을 kebab-case 로 입력하세요.'
       })
       .then(({ name }) => {
         if (!name) {
           throw new Error('컴포넌트 이름을 입력하세요!')
         }
         if (new RegExp(/[^a-z\-]/).test(name)) {
-          throw new Error('컴포넌트 이름은 snake-case 이어야 합니다.')
+          throw new Error('컴포넌트 이름은 kebab-case 이어야 합니다.')
         }
         return { name: name, pascal: toPascalCase(name), args }
       })
